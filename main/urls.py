fe20575c969b38777a04a8c8fc4df832a145518f
vaddddie <v_bykovski@mail.ru>
2022-10-18 11:06:33 +0300

    Price page added

--- main/urls.py
+++ main/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index.as_view(), name="index"),
+    path('prices/', views.shop.as_view(), name="shop"),
 ]
--- main/views.py
+++ main/views.py
@@ -29,3 +29,28 @@ class index(View):
         }
         return render(request, self.template_name, context)
 
+
+class shop(View):
+    items = Item.objects.all()
+    template_name = 'main/shop.html'
+
+    def post(self, request, *args, **kwargs):
+        letter_form = LettersForm(request.POST)
+        context = {
+            'items': self.items,
+            'letter_form': letter_form,
+        }
+
+        if letter_form.is_valid():
+            letter_form.save()
+            return redirect('index')
+
+        return render(request, self.template_name, context)
+
+    def get(self, request, *args, **kwargs):
+        letter_form = LettersForm()
+        context = {
+            'items': self.items,
+            'letter_form': letter_form,
+        }
+        return render(request, self.template_name, context)
